1a7978bb0549f9c96d93b774763a50ab959be017
maxkrieger04 <maxkrieger04@gmail.com>
2023-04-02 13:52:50 -0500

    Your commit message here

---
+++ email_extension/email_extension_code/chatbot.py
@@ -0,0 +1,23 @@
+import openai
+
+openai.api_key = "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" #Open AI API Key to make requests 
+
+def generate_response(email, optionalInformation=None):
+    prompt = f"Your email: {email}"
+    if optionalInformation:
+        prompt += f"\nOptional information to put in email: {optionalInformation}"\
+        
+    prompt += "\nOpenAI's response:"
+    
+    model = "text-davinci-003"
+    
+    response = openai.Completion.create(
+        engine=model,
+        prompt=prompt,
+       max_tokens=1000, #Max email length 
+        n=1, #Will generate one email responce
+        stop=None, #Parameter specifies no stop sequence
+        temperature=0.7, #Tempature of 0.7 chosen to prevent text from sounding robotic, but also remaining professional
+    )
+    
+    return response.choices[0].text.strip()
